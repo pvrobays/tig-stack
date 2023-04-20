bb6df9d4cf1c1fdffd4107ab1880d8361647c043
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2023-04-20 17:15:21 +0200

    add support for influxdb 1.x version api

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,4 @@ export DOCKER_INFLUXDB_INIT_HOST=$DOCKER_INFLUXDB_INIT_HOST
 
 # Conducts initial InfluxDB using the CLI
 influx setup --skip-verify --bucket ${DOCKER_INFLUXDB_INIT_BUCKET} --retention ${DOCKER_INFLUXDB_INIT_RETENTION} --token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} --org ${DOCKER_INFLUXDB_INIT_ORG} --username ${DOCKER_INFLUXDB_INIT_USERNAME} --password ${DOCKER_INFLUXDB_INIT_PASSWORD} --host http://${DOCKER_INFLUXDB_INIT_HOST}:8086 --force
+#influx setup --skip-verify --bucket telegraf --retention 4d --org pvr --host http://influxdb:8086 --force
